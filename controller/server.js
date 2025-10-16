d0f64e8cc58b580e1afded14be46419abbe84607
Uplink036 <olsj21@student.bth.se>
2025-10-16 09:56:48 +0200

    Add ability to fetch new task

--- controller/server.js
+++ controller/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+let currentTask = 0;
+
 app.set('view engine', 'pug');
 app.disable('view cache');
 app.use(express.json());
@@ -9,6 +11,11 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/api/task', (req, res) => {
+  res.json({ task: currentTask });
+  currentTask += 1
 });
 
 app.listen(port, () => {
